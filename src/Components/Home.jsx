186489ae0dc012aaa39be4930acb9d5da17022b6
DariyaSherbakova <77777daria@gmail.com>
2023-06-20 09:34:08 +0200

    added hero section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="hero min-h-screen bg-opacity-60 stylehero">
-    <div className="hero-content   text-center text-neutral-content ">
+    <div className="hero min-h-screen stylehero">
+    <div className="w-full hero-content lolo text-center text-neutral-content ">
       <div className="max-w-md">
         <h1 className="text-5xl font-bold ">Find a home that suits your lifestyle.</h1>
-        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <p className="py-6">Find your place with an immersive photo experience and the most listing, including things you won't find anywhere else.</p>
         <button className="btn btn-primary">Get Started</button>
       </div>
     </div>
@@ -15,3 +15,5 @@ function Home() {
 }
 
 export default Home
+
+
